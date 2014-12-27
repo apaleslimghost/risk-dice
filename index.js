3549b89844b499d12db19fa4307408060f3bdf07
Matt Brennan <mattyb1000@gmail.com>
2014-12-27 19:20:08 +0000

    all of the things

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-document.title = 'Risk dice';
-
 var React = require('react');
 var {
   dice,
@@ -8,12 +6,29 @@ var {
   risk
 } = require('risk-dice');
 
+function sub(a, b) {
+  return b - a;
+}
+
+function diceUnicode(n) {
+  return String.fromCharCode(0x2680 + n - 1);
+}
+
+var Dice = React.createClass({
+  render() {
+    return <div className={"dice " + this.props.className}>
+      {this.props.dice.map((die, i) => <div className="die" key={i}>{diceUnicode(die)}</div>)}
+    </div>;
+  }
+});
+
 var App = React.createClass({
   getInitialState() {
     return {
       attack: 0,
       defend: 0,
-      casualties: false
+      casualties: false,
+      dice: false
     };
   },
 
@@ -24,22 +39,42 @@ var App = React.createClass({
   },
 
   roll() {
+    var attack = dice(6, this.state.attack).sort(sub);
+    var defend = dice(6, this.state.defend).sort(sub);
+
     this.setState({
-      casualties: risk(this.state.attack, this.state.defend)
+      casualties: casualties(battle(attack, defend)),
+      dice: {attack, defend}
     });
   },
 
   render() {
-    return <div>
-      <input type="number" onChange={this.changeNumber} name="attack"/>
-      <input type="number" onChange={this.changeNumber} name="defend"/>
-      <button type="button" onClick={this.roll}>Roll</button>
-      <ul>
-        <li>Attacker lost {this.state.casualties.attacker}</li>
-        <li>Defender lost {this.state.casualties.defender}</li>
-      </ul>
+    return <div style={{width: "320px"}} className="center-block">
+      <h1>Risk dice roll</h1>
+      <div className="row">
+      <div className="col-xs-6 form-group">
+        <input type="number" onChange={this.changeNumber} name="attack" className="form-control input-lg attack" placeholder="Attack dice"/>
+      </div>
+      <div className="col-xs-6 form-group">
+        <input type="number" onChange={this.changeNumber} name="defend" className="form-control input-lg" placeholder="Defence dice"/>
+      </div>
+      <div className="col-xs-12">
+        <button type="button" onClick={this.roll} className="btn btn-primary btn-block">Roll</button>
+      </div>
+      {this.state.casualties && <div>
+       <h2 className="col-xs-6 attack">Attacker lost {this.state.casualties.attacker}</h2>
+       <h2 className="col-xs-6 defend">Defender lost {this.state.casualties.defender}</h2>
+      </div>}
+      {this.state.dice && <div>
+       <Dice dice={this.state.dice.attack} className="col-xs-6 attack"/>
+       <Dice dice={this.state.dice.defend} className="col-xs-6 defend"/>
+      </div>}
+      </div>
     </div>;
   }
 });
 
 React.render(<App/>, document.documentElement);
+document.title = 'Risk dice';
+require('bootstrap/dist/css/bootstrap.css');
+require('./style.css');
